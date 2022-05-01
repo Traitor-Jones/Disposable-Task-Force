81af253202d1edbff7a6f31fa71fba477b05d6fd
Shana Tirtawidjaja <stirt001@ucr.edu>
2022-05-01 03:11:58 -0700

    adjusted spawning, added multiple hazard options

--- Assets/Scripts/spawn.cs
+++ Assets/Scripts/spawn.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
+ [System.Serializable] //<< lets you edit in inspector
+public struct SpawnOb{
+  public GameObject item;
+  public Quaternion Rotation; 
+}
 
 public class spawn : MonoBehaviour{
-    public GameObject Asteroid;
-
+public SpawnOb[] enHazards;
     private void Start()
     {
-    StartCoroutine(RandomSpawnEverySeconds(5.0f));
+    StartCoroutine(RandomSpawnEverySeconds(2.0f));
     }
 
 IEnumerator RandomSpawnEverySeconds(float seconds)
@@ -17,23 +20,25 @@ IEnumerator RandomSpawnEverySeconds(float seconds)
 
   while (true)
   {
-    float randomX = Random.Range(0f, 50f);
-    float randomY = Random.Range(-50f, 50f);
+    float randomX = Random.Range(0f, 1.1f);
+   float randomY = Random.Range(0f, 1.1f);
+   int index = Random.Range(0,6);
+   //Debug.Log(index);
 
-     Vector3 v3Pos = Camera.main.ViewportToWorldPoint(new Vector3(randomX, randomY, 30.0f));
+     Vector3 v3Pos = Camera.main.ViewportToWorldPoint(new Vector3(randomX, 0.5f, 2200.0f));
 
-    Instantiate(Asteroid, v3Pos, Asteroid.transform.rotation);
+    Instantiate(enHazards[index].item, v3Pos, enHazards[index].Rotation);
 
 
     yield return new WaitForSeconds(seconds);
   }
 }
+}
 
 
 
-
-
- /*   void Update(){
+ /* just some notes  
+ void Update(){
         SpawnObjectAtPosition(0f, 500f, 0f);
     }
 
@@ -48,8 +53,6 @@ IEnumerator RandomSpawnEverySeconds(float seconds)
     }  */
 
 
-}
-
 /*
  void Update()
     {
@@ -63,8 +66,5 @@ IEnumerator RandomSpawnEverySeconds(float seconds)
             clone.timeoutDestructor = 5;
         }
     }
-
-
-
 
 */
