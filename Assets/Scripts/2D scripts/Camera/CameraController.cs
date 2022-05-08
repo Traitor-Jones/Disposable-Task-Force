9d61d2153644be9b93f468e8aa2bf6fb1fc05d65
Isaac Kang <configuration>
2022-05-08 12:06:59 -0700

    Fixed camera movement to follow player and not by rooms

--- Assets/Scripts/2D scripts/Camera/CameraController.cs
+++ Assets/Scripts/2D scripts/Camera/CameraController.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    // Room camera
     [SerializeField] private float speed;
     public float currPosX;
     private Vector3 velocity = Vector3.zero;
 
+    // Follow player
+    [SerializeField] private Transform player;
+
     private void Update()
     {
-        transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currPosX, transform.position.y, transform.position.z), ref velocity, speed);
+        // Room Camera
+        //transform.position = Vector3.SmoothDamp(transform.position, new Vector3(currPosX, transform.position.y, transform.position.z), ref velocity, speed);
+
+        // Follow Player
+        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
     }
 
     public void MoveToNewRoom(Transform newRoom)
