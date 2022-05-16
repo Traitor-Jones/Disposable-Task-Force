fc607166643220da5dff1796678848a5d8aae748
Jaired Jawed <jairedjawed@icloud.com>
2022-05-16 10:15:38 -0700

    Added explosion when colliding with planet

--- Assets/Scripts/PlanetCollision.cs
+++ Assets/Scripts/PlanetCollision.cs
@@ -5,18 +5,30 @@ using UnityEngine;
 public class PlanetCollision : MonoBehaviour
 {
     public GameObject playerShip;
+    public GameObject shipExplosion;
+    Vector3 originalPlayerShipPosition;
+
+    private Vector3 shipExplosionPos;
     private ShipHealth shipHealth;
 
     // Start is called before the first frame update
     void Start()
     {
+        originalPlayerShipPosition = playerShip.transform.position;
         shipHealth = playerShip.GetComponent<ShipHealth>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // set the ship's explosion position to the ship's position
+        shipExplosionPos = playerShip.transform.position;
+    }
 
+    void RevivePlayer() {
+        playerShip.transform.position = originalPlayerShipPosition;
+        playerShip.SetActive(true);
+        shipHealth.ResetHealth();
     }
 
     void OnTriggerEnter(Collider other)
@@ -25,5 +37,14 @@ public class PlanetCollision : MonoBehaviour
         // we will have the ship explode instead at some point
         shipHealth.TakeDamage(shipHealth.getHealth());
         playerShip.GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+        // set the explosion position to the ship's position and activate it
+        shipExplosion.transform.position = shipExplosionPos;
+        shipExplosion.SetActive(true);
+
+        playerShip.SetActive(false);
+
+        // wait a few seconds and then revive the player
+        Invoke("RevivePlayer", 3);
     }
 }
